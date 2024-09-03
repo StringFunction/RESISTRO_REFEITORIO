ffc608d68a1c75b377e5c9286deed428694cea48
cleciocarmais <franciscoclecioti@gmail.com>
2024-09-03 17:51:53 -0300

    criando page funcionario

---
+++ src/Navbar/footer.jsx
@@ -0,0 +1,11 @@
+function footer(){
+    return (
+        <>
+
+        <div className="bg-neutral-400 h-28	 px-2 flex justify-center	items-center	">
+         <h1 >Desenvolvido por StringFunction</h1>
+        </div>
+        </>
+    )
+}
+export default footer
