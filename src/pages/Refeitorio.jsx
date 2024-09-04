ee1aa780ae3e6ae64eef78862245bb4496919c67
cleciocarmais <franciscoclecioti@gmail.com>
2024-09-04 18:05:41 -0300

    criando card

--- src/pages/Refeitorio.jsx
+++ src/pages/Refeitorio.jsx
@@ -1,8 +1,8 @@
 import { useState } from "react"
-
+import CardFun from "./componentes/CardFuncionario"
 function Refeitorio() {
 const [matricula, setMatricula] = useState("")
-const [resultado, setS] = useState()
+const [resultado, setS] = useState("")
 const dados = [
     {"matricula" : "123", "nome" : "clecio"},
     {"matricula" : "456", "nome" : "silva"},
@@ -11,9 +11,16 @@ const dados = [
 ]
 
 function MOSTRA(){
- const resultado = dados.find(dado => dado.matricula == matricula)
- setS(resultado)
- 
+ const resulta = dados.find(dado => dado.matricula == matricula)
+
+ if (resulta){
+     setS(JSON.stringify(resulta)) 
+     console.log(resulta);
+    
+ }else{
+    setS("USUARIO N ENCONTRADO ");
+    
+ }
     
     
 }
@@ -21,11 +28,11 @@ function MOSTRA(){
         <>
         <div className="flex  flex-col gap-9 items-center justify-center bg-gray-300 h-96">
 
-            <h1 className="text-5xl">REGISTRA DE FUNCIONARIO</h1>   
+            <h1 className="text-5xl">CONDO DO FORRO </h1>   
             <div className="flex flex-col gap-9"> 
                 <input className="w-96 text-center h-12 rounded-lg border-none	"  id="nome"type="text" onChange={(e) => setMatricula(e.target.value)} placeholder="DIGITE MATRICULA DO FUNCIONARIO" />
                 <button className="border-solid bg-lime-600 h-12" type="button" onClick={MOSTRA}>Consulta</button>
-                <h1><h1>{resultado ? "Matrícula encontrada" : "Matrícula não encontrada"}</h1></h1>
+                <CardFun user = {resultado}/>
               
             </div>
             <div>
---
+++ src/pages/componentes/CardFuncionario.jsx
@@ -0,0 +1,11 @@
+function CardFun({user}){
+
+    return(
+        <>
+        <h1> nome : {user}</h1>
+        </>
+    )
+
+}
+
+export default CardFun
