056f02b7f8dd4562232ca499221951d67552ce49
cleciocarmais <franciscoclecioti@gmail.com>
2024-10-21 14:08:25 -0300

    feito resposividade na pagina telaFrequetandor

--- src/pages/TelaCadFrequentador.tsx
+++ src/pages/TelaCadFrequentador.tsx
@@ -5,19 +5,21 @@ export default function TelaCadFrequen() {
     return (
         <>
             <div id="TelaFundo" className="flex justify-center items-center">
-                <video src={video} autoPlay muted loop className="absolute top-0 left-0 w-full h-full object-cover" ></video>
+                <video src={video} autoPlay muted loop className="relative top-0 left-0 w-full h-[760px] object-cover" ></video>
 
-                <div id="cardFundoBlack" className="md:w-[450px] md:h-[550px] bg-black/50 absolute flex flex-col justify-center items-center top-3 tracking-[4px] backdrop-blur-sm
- rounded-2xl text-white h-[500px] ">
-                <div id="tituto" className="md:text-[20px] relative md:-top-10 text-white uppercase text-center text-[15px] text-wrap w-[400px]">Cadastro Funcionario Frequentador</div>
+                <div id="cardFundoBlack" className="md:w-[450px] md:h-[550px] bg-black/50 absolute flex flex-col justify-center items-center md:top-10 tracking-[4px] backdrop-blur-md
+
+
+ rounded-2xl text-white h-[500px] top-32 ">
+                <div id="tituto" className="md:text-[20px] relative md:-top-3 text-white uppercase text-center text-[15px] text-wrap w-[400px]">Cadastro Funcionario Frequentador</div>
                 <div id="cardForm">
                     <form action="" className="flex flex-col ">
                         <label className="text-white text-[20px] mb-2 " htmlFor="">Matricula : </label>
                         <input className="bg-transparent border-b-2  mb-2" type="text" />
                         <label className="text-white text-[20px] mb-2 " htmlFor="">Nome : </label>
                         <input className="bg-transparent border-b-2  mb-2" type="text" />
-                        <label className="text-white text-[20px] mb-2 " htmlFor="">Empresa</label>
-                        <select name="" className="bg-transparent mb-3"id="">
+                        <label className="text-white text-[20px] mb-2 " htmlFor="">Empresa : </label>
+                        <select name="" className="bg-transparent mb-3 border-b-2"id="">
                             <option value="" className="text-black">Tstes</option>
                             <option value="" className="text-black">Tstes</option>
                             <option value="" className="text-black">Tstes</option>
@@ -29,7 +31,7 @@ export default function TelaCadFrequen() {
                         <input className="bg-transparent border-b-2  mb-2" type="text" />
                         <label className="text-white text-[20px] mb-2 " htmlFor="">Cargo : </label> 
                         <input className="bg-transparent border-b-2  mb-2" type="text" />
-                        <input className="bg-transparent border-b-2  mb-2 p-2 hover:cursor-pointer" type="submit" value="Cadastrar" />
+                        <input className="bg-transparent border-b-2  mb-2 p-2 hover:cursor-pointer hover:tracking-[8px] duration-500 " type="submit" value="Cadastrar" />
                     </form>
                 </div>
                 
