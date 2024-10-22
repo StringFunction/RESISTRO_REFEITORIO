c9bb642dca6feb08b3c0c903a93909f75058270a
cleciocarmais <franciscoclecioti@gmail.com>
2024-10-22 17:46:38 -0300

    estilos telaFre

--- src/pages/TelaCadFrequentador.tsx
+++ src/pages/TelaCadFrequentador.tsx
@@ -1,10 +1,13 @@
-import React from "react";
+import React, { useRef } from "react";
 import video from "./../assets/video.mp4";
 import { useState } from "react";
+import { ToastContainer, toast } from "react-toastify";
+import api from "../service/api";
 
 
 export default function TelaCadFrequen() {
     const empresas = [
+        "",
         "CONTERRANEA MACAIBA",
         "CONTERRANEA MATRIZ",
         "CONTERRANEA MOSSORO",
@@ -31,9 +34,47 @@ export default function TelaCadFrequen() {
     const [empresa, setEmpresa] = useState("")
     const [setor, setSetor] = useState("")
     const [cargo, setCargo] = useState("")
+    const form = useRef(null)
+    
+ 
     
     function CadastroF(params) {
         params.preventDefault()
+        const inputs = [...params.target.querySelectorAll("input")]
+        const select = params.target.querySelector("select");
+        if (!inputs.every(input => input.value) || !select.value) {
+            console.log(select.value);
+            
+            if (!select.value) {
+                select.classList.add("border-red-800")
+            }else{
+                select.classList.remove("border-red-800")
+
+            }
+            inputs.forEach((e, index) => {
+                
+                if(!e.value){
+                    e.classList.add("border-red-800");
+                  
+                }else{
+                    e.classList.remove("border-red-800");
+
+                }
+            })
+        return toast.info("Preencha os Campos")
+        }
+        const dados = {
+            matricula : matricula,
+            nome : nome,
+            empresa : empresa,
+            setor : setor,
+            cargo : cargo,
+            Optante : "frequentado"
+
+        }
+
+        
+        
         console.log("NOME : " + nome);
         console.log("MATRICULA : " + matricula);
         console.log("EMPRESA : " + empresa);
@@ -53,36 +94,45 @@ export default function TelaCadFrequen() {
                 <video src={video} autoPlay muted loop className="relative  w-[1500px] h-[760px] object-cover"></video>
                 </div>
 
-                <div id="cardFundoBlack" className="md:w-[450px] md:h-[550px] bg-black/50 absolute flex flex-col justify-center items-center md:top-10 tracking-[4px] backdrop-blur-md
-
-
- rounded-2xl text-white h-[500px] top-32 ">
+                <div id="cardFundoBlack" className="md:w-[450px] md:h-[550px] bg-black/50 absolute flex flex-col justify-center items-center md:top-10 tracking-[4px] backdrop-blur-md rounded-2xl text-white h-[500px] top-32 ">
                 <div id="tituto" className="md:text-[20px] relative md:-top-3 text-white uppercase text-center text-[15px] text-wrap w-[400px]">Cadastro Funcionario Frequentador</div>
                 <div id="cardForm">
-                    <form onSubmit={CadastroF} className="flex flex-col ">
+                    <form onSubmit={CadastroF} className="flex flex-col " ref={form}>
                         <label className="text-white text-[20px] mb-2 " htmlFor="">Matricula : </label>
                         <input className="bg-transparent border-b-2  mb-2" onChange={(e) => setMatricula(e.target.value)} type="text" />
 
                         <label className="text-white text-[20px] mb-2 " htmlFor="">Nome : </label>
                         <input className="bg-transparent border-b-2  mb-2"onChange={(e) => setNome(e.target.value)} type="text" />
 
-                        <label className="text-white text-[20px] mb-2 " htmlFor="">Empresa : </label>
+                        <p className="text-white text-[20px] mb-2 ">Empresa : </p>
                         <select name="" className="bg-transparent mb-3 border-b-2 text-[20px]"id="" onChange={(e) => setEmpresa(e.target.value)} value={empresa}>
-                            {empresas.map((empresa,index) =><option value={empresa} key={index} className="text-black">{empresa}</option>)}
+                            {empresas.map((empresa,index) =><option value={empresa} key={index} className="text-black bg-transparent">{empresa}</option>)}
                            
                         </select>
                         <label className="text-white text-[20px] mb-2 " htmlFor="">Setor : </label>
                         <input className="bg-transparent border-b-2  mb-2" type="text" onChange={(e) => setSetor(e.target.value)} />
                         <label className="text-white text-[20px] mb-2 " htmlFor="">Cargo : </label> 
                         <input className="bg-transparent border-b-2  mb-2" type="text" onChange={(e) => setCargo(e.target.value)}/>
+                        <div>
+
+                        </div>
                         <input className="bg-transparent border-b-2  mb-2 p-2 hover:cursor-pointer hover:tracking-[8px] duration-500 " type="submit" value="Cadastrar" />
                     </form>
                 </div>
                 
                 
                 </div>
+                {/* <div className="w-[400px] h-[500px] bg-black">
+                    <div id="taco">
+
+                    </div>
+                    <div id="bola">
+
+                    </div>
+                </div> */}
             </div>
-        
+
+        <ToastContainer></ToastContainer>
         </>
     )
     
