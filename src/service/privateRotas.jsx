e8471ef1788b9f86ecf4738c8e9462260e480daf
cleciocarmais <franciscoclecioti@gmail.com>
2024-10-25 15:08:15 -0300

    problema n resolvido

--- src/service/privateRotas.jsx
+++ src/service/privateRotas.jsx
@@ -1,28 +1,30 @@
 import { Contextapp } from "../context/ContextAuth";
 import { useContext, useEffect } from "react";
 import { Navigate, Outlet } from "react-router-dom";
-
+import { jwtDecode } from "jwt-decode";
 
 function PrivateRota(){
-
+  
   const isLogado = localStorage.getItem("token")
   const {nivel} = useContext(Contextapp)
   console.log("aqui estou mais um" + nivel);
   
-  
   if (!!isLogado) {
-    if (nivel == 3){
-      return <Outlet></Outlet>
-    } else {
-      alert("aqui estou");
-      return <Navigate to="Refeitorio"></Navigate>
-    }
-  } else{
-    console.log("aqui dentro de acessao de token invali ");
+    const dados = jwtDecode(isLogado);
     
-    return <Navigate to="/Login"></Navigate>
-  }
 
+    if (dados.nivel !== 3) {
+        alert("ola mundo");
+        return <Navigate to="/Refeitorio"></Navigate>
+    }
+
+    return (
+        <Outlet />
+    );
+} else {
+    console.log("Token de acesso inválido");
+    return <Navigate to="/Login" />;
+}
   // return !!isLogado ? <Outlet /> : <Navigate to="/Login"></Navigate>
 
 }
